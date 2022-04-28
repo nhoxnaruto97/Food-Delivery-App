279290cc8de9d4bb6c165f3a712d9bedbfbc39ba
NguyenCD <dinhnguyen.k15@gmail.com>
2022-04-28 14:34:57 +0700

    [Implement] - Implement StateProvider and Reducer for Authentication

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import reducer from "./context/reducer";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initialState";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </Router>
 );
